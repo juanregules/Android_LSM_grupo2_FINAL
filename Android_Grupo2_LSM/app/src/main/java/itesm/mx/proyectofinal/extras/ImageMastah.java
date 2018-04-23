67c3bc95c6bada3dd193d4cc7d570644c06ab19b
juanregules <33490910+juanregules@users.noreply.github.com>
2018-04-23 14:20:06 -0500

    cambios de martin
    pequeños cambios de martin en el "profile"

--- Android_Grupo2_LSM/app/src/main/java/itesm/mx/proyectofinal/extras/ImageMastah.java
+++ Android_Grupo2_LSM/app/src/main/java/itesm/mx/proyectofinal/extras/ImageMastah.java
@@ -11,9 +11,14 @@ import java.io.ByteArrayOutputStream;
 public class ImageMastah {
 
     public static Bitmap fromByteArrayToBitmap(byte[] foto){
-        return BitmapFactory.decodeByteArray(foto, 0, foto.length);
+        if(foto != null)
+            return BitmapFactory.decodeByteArray(foto, 0, foto.length);
+        else
+            return null;
     }
     public static byte[] fromBitmapToByteArray(Bitmap foto){
+        if(foto == null)
+            return null;
         Bitmap b = foto;
         ByteArrayOutputStream s = new ByteArrayOutputStream();
         b.compress(Bitmap.CompressFormat.PNG, 100, s);
@@ -25,6 +30,8 @@ public class ImageMastah {
         Drawable d;
 
         d = ivFoto.getDrawable();
+        if(d.getIntrinsicHeight() < 0 || d.getIntrinsicWidth() < 0)
+            return null;
         b = Bitmap.createBitmap(d.getIntrinsicWidth(), d.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
         Canvas canvas = new Canvas(b);
         d.setBounds(0,0,canvas.getWidth(), canvas.getHeight());
